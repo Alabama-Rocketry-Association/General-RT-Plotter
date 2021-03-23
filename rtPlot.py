600fda1f00f7c0e0edf1b18f356cbb6290611827
Jonathan Stuecker <jpstuecker@crimson.ua.edu>
2021-03-23 13:35:09 -0500

    it actually plots now asynchronously, just doesn't animate it

--- rtPlot.py
+++ rtPlot.py
@@ -9,22 +9,22 @@ data = list()
 fig = plt1.figure()
 ax = Axes3D(fig)
 
-async def gen():
+async def gen():    #this is mocking how our data will be recieved from the radio
     for i in range(10):
         yield (i, i*i, i*i*i)
 
 async def main():
     async for i in gen():
         data.append(i)
-        e, n, u = map(list, zip(*data))
-        #ax.scatter(e, n, u)
+        e, n, u = map(list, zip(*data))     #transpose data into e, n, u, coordinates
+        ax.scatter(e, n, u)
         print(e, n, u)
-        #plt.show()
 
 
 loop = asyncio.get_event_loop()
 try:
     loop.run_until_complete(main())
+    plt.show()
 finally:
     loop.run_until_complete(loop.shutdown_asyncgens())
     loop.close()
